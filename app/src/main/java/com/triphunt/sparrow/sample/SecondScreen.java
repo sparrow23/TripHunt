46b2086a81b47d0269fd36c32d716732e898d6dd
sparrow23 <mishra.anuj22@gmail.com>
2016-08-02 20:03:36 +0530

    after updating fragment

--- app/src/main/java/com/triphunt/sparrow/sample/SecondScreen.java
+++ app/src/main/java/com/triphunt/sparrow/sample/SecondScreen.java
@@ -1,14 +1,23 @@
 package com.triphunt.sparrow.sample;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Toast;
+
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 
 
+
 public class SecondScreen extends AppCompatActivity {
+
+   // public LoginFragment loginFragment = new LoginFragment();
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,10 +26,10 @@ public class SecondScreen extends AppCompatActivity {
 
         setContentView(R.layout.second_layout);
 
-
     }
+
     public void click_continue(View view) {
         String response = "sorry sir we haven't worked so far!!!";
-        Toast.makeText(this, response, Toast.LENGTH_LONG).show();
+        Toast.makeText(this, response, Toast.LENGTH_SHORT).show();
     }
 }
