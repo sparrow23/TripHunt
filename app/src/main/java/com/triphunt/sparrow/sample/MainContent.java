d7373c6288f0d5eca263388461f0f7ddeea4fbe9
sparrow23 <mishra.anuj22@gmail.com>
2017-04-04 14:06:41 +0530

    (rewrite) adding place details by Aadarsh

--- app/src/main/java/com/triphunt/sparrow/sample/MainContent.java
+++ app/src/main/java/com/triphunt/sparrow/sample/MainContent.java
@@ -1,7 +1,7 @@
 package com.triphunt.sparrow.sample;
 
-import android.content.Context;
-import android.net.Uri;
+//import android.content.Context;
+//import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
